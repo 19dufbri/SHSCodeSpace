706583fd362d2fe20d71516129610b86f0068f78
Brian Duffy <bd1112@roadrunner.com>
2017-10-31 16:13:31 +0000

    Added Content to Files
    Signed-off-by: Brian Duffy <bd1112@roadrunner.com>

---
+++ index.js
@@ -0,0 +1,19 @@
+let on = true;
+
+function toggleCutout() {
+    let elements = document.getElementsByClassName("cutout");
+    
+    console.log("Toggling");
+    
+    for (let i = 0; i < elements.length; i++) {
+        if (on) {
+            elements[i].style = "background: black";
+            on = false;
+        } else {
+            elements[i].style = "background: yellow";
+            on = true;
+        }
+    }
+}
+
+toggleCutout();
